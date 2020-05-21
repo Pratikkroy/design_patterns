4de170ca1c320c61744187cb46239f5c0bad2bd2
Pratik Roy <roypra@amazon.com>
2020-05-21 16:40:46 +0530

    testing commit on master branch

--- src/com/pratikkroy/design_patterns/Main.java
+++ src/com/pratikkroy/design_patterns/Main.java
@@ -1,7 +1,7 @@
 package com.pratikkroy.design_patterns;
 
 import com.pratikkroy.design_patterns.structural.StructuralPatternDemo;
-
+// testing commit on master branch
 public class Main {
     public static void main(String[] args){
 //        CreationalPatternDemo.existing_use();
